474715adfae7c9252fb7c935b1081f0400e68abb
Muhammad Ahmad <93470482+ahmedevv@users.noreply.github.com>
2022-12-16 20:28:13 +0500

    Update trailing-stoploss.py

--- trailing-stoploss.py
+++ trailing-stoploss.py
@@ -1,19 +1,3 @@
-"""
-Trailing Stoploss
-Author: TraderPy
-
-Youtube: https://www.youtube.com/channel/UC9xYCyyR_G3LIuJ_LlTiEVQ/featured
-
-Website: https://traderpy.com/
-
-Disclaimer
-Trading the financial markets imposes a risk of financial loss. TraderPy is not responsible for any financial losses
-that viewers suffer. Content is educational only and does not serve as financial advice. Information or material is
-provided ‘as is’ without any warranty.
-
-Past trading results do not indicate future performance. Strategies that worked in the past may not reflect the same
-results in the future.
-"""
 
 # import libraries
 import MetaTrader5 as mt5
